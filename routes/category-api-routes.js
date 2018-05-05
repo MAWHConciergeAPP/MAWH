af0bab98c6a15444b83075643766e8c926ceeee1
Ashley Moore <mooreashleyplease@gmail.com>
2018-05-05 13:57:41 -0400

    routes written; server.js added; testing dir added

---
+++ routes/category-api-routes.js
@@ -0,0 +1,20 @@
+var db = require("../models");
+
+module.exports = function(app) {
+    app.get("/api/categories", function(req, res) {
+        db.Category.findAll({
+            include: [db.Activity]
+        }).then(function(dbCategory) {
+            res.json(dbCategory);
+        });
+    });
+
+    app.get("/api/categories/:id", function(req, res) {
+        db.Category.findOne({
+            where: {id: req.params.id},
+            include: [db.Actvity]
+        }).then(function(dbCategory) {
+            res.json(dbCategory);
+        });
+    });
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var db = require("./models");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+require("./routes/activity-api-routes.js");
+require("./routes/category-api-routes.js");
+require("./routes/contact-api-routes.js");
+require("./routes/faq-api-routes.js");
+require("./routes/user-api-routes.js");
+
+db.sequelize.sync({force: true}).then(function() {
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
+});
