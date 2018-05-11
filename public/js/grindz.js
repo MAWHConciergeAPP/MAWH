6705dd7c54819c4d58c73a446f82403e02b6328b
kayla mcfadden <kmcfadden0741@gmail.com>
2018-05-11 10:56:28 -0400

    grindz page closergit add .!

--- public/js/grindz.js
+++ public/js/grindz.js
@@ -1,20 +1,30 @@
+
 $(document).ready(function(){
      
        // grindz form
  
+
         $("#local-food").change(function() {
             var keyword = $(this).val();
             console.log(keyword);
 
-            $("#gallery-left").preappend("<img id='onos' src='../public/img/onos.jpg'>");
-            $("#gallery-right").preappend("<img id='onos' src='../public/img/onos.jpg'>");
-    
             $.get("/api/grindz/" + keyword, function(data) {
                 console.log(data);
+                $(".gallery").append(data.img_url);
+                // $(".gallery-right").append(data.img_url);
+            
                 for (var i = 0; i < data.length; i++) {
-                    $("#desc").append("<p>" +  + "</p>");
-                    $("#desc").append("<p>" +  + "</p>");
-                    $("#desc").append("<p>" +  + "</p>");
+                    $(".desc").append("<p>" + data.name + "<br>" 
+                    + data.streetAddress + "<br>" 
+                    + data.city + "<br>" 
+                    + data.island + "<br>" 
+                    + data.phone + "<br>" 
+                    + data.hours + "<br>"
+                    + data.website_url + "<br>"
+                    + data.keyword + "<br>"
+                    + data.price + "<br>"
+                    + data.notes + "</p>");
+                    
                 }
     
             });
