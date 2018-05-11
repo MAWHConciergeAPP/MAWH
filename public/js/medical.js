42cc4c9a6262342dc431338b4638229680099f02
Ashley Moore <mooreashleyplease@gmail.com>
2018-05-11 11:56:09 -0400

    medical appearing without website

--- public/js/medical.js
+++ public/js/medical.js
@@ -1,47 +1,28 @@
-/*----------------------medical facilities table------------------------------------*/
+$(document).ready(function() {
 
-/*axios.get('/api/clinics', {
-    params: {
-    id: 2
-    }
-})
-    .then(function (response) {
-      console.log(response);
-      $('#medicalTable tr:last').after(`<tr>${response.facilityName}</tr><tr>${response.address}</tr><tr>${response.phoneNumber}</tr><tr>${response.notes}</tr>`);
-    })
-    .catch(function (error) {
-    
-    });   */
-
-    $(document).ready(function() {
-        $("#medical-table").change(function() {
-            var keyword = $(this).val();
-            console.log(keyword);
-    
-            $.get("/api/clinics" + keyword, function(data) {
-                console.log(data);
-                $("#medical-tbody").children().remove();
-                data.forEach(function(result) {
-                    var tr = $("<tr>");
-                    tr.append("<td>" + result.facilityName + "</td>");
-                    tr.append("<td>" + result.address + ", " + result.city + "</td>");
-                    tr.append("<td>" + result.phoneNumber + "</td>");
-                    tr.append("<td>" + result.notes + " hours</td>");
-                    tr.appendTo("#tbody");
-                });
-            });
+    $.get("/api/medicals", function(data) {
+        console.log(data);
+        $("#medical-tbody").children().remove();
+        data.forEach(function(result) {
+            var tr = $("<tr>");
+            tr.append("<td>" + result.facility + "</td>");
+            tr.append("<td>" + result.streetAddress + ", " + result.city + "</td>");
+            tr.append("<td>" + result.phone + "</td>");
+            tr.append("<td><a href = '" + result.url + "' target='_blank'></td>");
+            tr.appendTo("#medical-tbody");
         });
     });
+});
 
 
 
 /*----------------------chapter contact table------------------------------------*/
 
-axios.get("/api/users/:id")
-    .then(function (response) {
-      console.log(response);
-      $('#chapter-table tr:last').after(`<tr>${response.chapter}</tr><tr>${response.phone}</tr>`);
-    })
-    .catch(function (error) {
-    
-    });   
+// axios.get("/api/users/:id")
+// .then(function (response) {
+//   console.log(response);
+//   $('#chapter-table tr:last').after(`<tr>${response.chapter}</tr><tr>${response.phone}</tr>`);
+// })
+// .catch(function (error) {
+
+// });   
