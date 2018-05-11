64422c24ef6b1f7f639eefeef95916df93815564
asterrypm <asterrypm@gmail.com>
2018-05-11 12:14:09 -0400

    updated url for medical table and js for contact table

--- public/js/medical.js
+++ public/js/medical.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
             tr.append("<td>" + result.facility + "</td>");
             tr.append("<td>" + result.streetAddress + ", " + result.city + "</td>");
             tr.append("<td>" + result.phone + "</td>");
-            tr.append("<td><a href = '" + result.url + "' target='_blank'></td>");
+            tr.append("<td><a href = '" + result.url + "' target='_blank'></a></td>");
             tr.appendTo("#medical-tbody");
         });
     });
@@ -16,13 +16,23 @@ $(document).ready(function() {
 
 
 
+
 /*----------------------chapter contact table------------------------------------*/
+$(document).ready(function() {
 
-// axios.get("/api/users/:id")
-// .then(function (response) {
-//   console.log(response);
-//   $('#chapter-table tr:last').after(`<tr>${response.chapter}</tr><tr>${response.phone}</tr>`);
-// })
-// .catch(function (error) {
+    $.get("/api/users/:id", function(data) {
+        console.log(data);
+        $("#chapter-table tr:last").children().remove();
+        data.forEach(function(result) {
+            var tr = $("<tr>");
+            tr.append("<td>" + result.chapter + "</td>");         
+            tr.append("<td>" + result.phone + "</td>");        
+            tr.appendTo("#chapter-table");
+        });
+    });
+});
 
-// });   
+
+
+
+
